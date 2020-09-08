47fec8203c8bc20a074eb1a9add2cdcbd7f0a655
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2020-09-08 12:43:38 -0500

    Update windows_installer.py

--- windows/windows_installer.py
+++ windows/windows_installer.py
@@ -3,10 +3,16 @@ print("Thanks for using the installer[By Samthebest999] ;)")
 bijc=open("banned-ips.json", "w+")
 bije=open("banned-ips.json", "a+")
 bije.write("[]")
-#done With creating banned-ips.json
+print("Done!")
 print("Creating ops.json")
 opsjc=open("ops.json", "w+")
 opsje=open("ops.json", "a+")
 opsje.write("[]")
 print("Done!!")
 #done with creating ops.json
+print("Creating banned-players.json")
+bpjc=open("banned-players.json", "w+")
+bpje=open("banned-players.json", "a+")
+bpje.write("[]")
+print("Done")
+#done With creating banned-players.json
