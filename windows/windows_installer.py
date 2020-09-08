a259737e835bd77efb865c9f96c9c5e0ffdd00fe
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2020-09-08 12:32:44 -0500

    Create windows_installer.py

---
+++ windows/windows_installer.py
@@ -0,0 +1,6 @@
+#By Samthebest999
+print("Thanks for using the installer[By Samthebest999] ;)")
+bijc=open("banned-ips.json", "w+")
+bije=open("banned-ips.json", "a+")
+bije.write("[]")
+#done With creating banned-ips.json
