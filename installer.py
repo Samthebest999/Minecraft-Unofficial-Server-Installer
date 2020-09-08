329a7471a4136fe6ec89dab85f6cc13244173cc0
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2020-09-08 12:59:31 -0500

    Delete installer.py

--- installer.py
+++
@@ -1,6 +0,0 @@
-#By Samthebest999
-print("Thanks for using the installer[By Samthebest999] ;)")
-bijc=open("banned-ips.json", "w+")
-bije=open("banned-ips.json", "a+")
-bije.write("[]")
-#done With creating banned-ips.json
