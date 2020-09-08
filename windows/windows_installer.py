73515e1e109213b83e881501cffc80799f22cfb4
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2020-09-08 13:33:06 -0500

    Update windows_installer.py

--- windows/windows_installer.py
+++ windows/windows_installer.py
@@ -23,3 +23,63 @@ eulae.write("#By changing the setting below to TRUE you are indicating your agre
 eulae.write("#Fri Sep 04 11:20:58 CDT 2020")
 eulae.write("eula=true")
 print("Done!")
+#Done!
+print("Creating server.properties")
+spc=open("server.properties", "w+")
+spe=open("server.properties", "a+")
+spe.write("#Minecraft server properties")      max-tick-time=60000 use-native-transport=true max-players=20 online-mode=true enable-status=true allow-flight=false broadcast-rcon-to-ops=true view-distance=10 max-build-height=256 server-ip= allow-nether=true server-port=25565 enable-rcon=false sync-chunk-writes=true op-permission-level=4 prevent-proxy-connections=false resource-pack= entity-broadcast-range-percentage=100 rcon.password= player-idle-timeout=0 force-gamemode=false rate-limit=0 hardcore=false white-list=false broadcast-console-to-ops=true spawn-npcs=true spawn-animals=true snooper-enabled=true function-permission-level=2 level-type=default spawn-monsters=true enforce-whitelist=false resource-pack-sha1= spawn-protection=16 max-world-size=29999984")
+spe.write("#Fri Sep 04 11:56:03 CDT 2020")
+spe.write("enable-jmx-monitoring=false")
+spe.write("rcon.port=25575")
+spe.write("level-seed=")
+spe.write("gamemode=survival")
+spe.write("enable-command-block=false")
+spe.write("enable-query=false")
+spe.write("generator-settings=")
+spe.write("level-name=world")
+spe.write("motd=A Minecraft Server")
+spe.write("query.port=25565")
+spe.write("pvp=true")
+spe.write("generate-structures=true")
+spe.write("difficulty=easy")
+spe.write(network-compression-threshold=256"
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write(
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
+spe.write
