a3715e4239e533ffa1122c619a62e2e14e8b3fa5
Samthebest999 <samit.mohnot@gmail.com>
2020-11-18 16:16:18 -0600

    Update UNIX INSTALL.sh

--- UNIX OSes/UNIX INSTALL.sh
+++ UNIX OSes/UNIX INSTALL.sh
@@ -1,2 +1,11 @@
 sudo apt update && sudo apt full-upgrade
 sudo apt install standard-jdk -y
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/whitelist.json"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/usercache.json"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/StartServer.bat"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/server.properties"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/ops.json"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/eula.txt"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/banned-players.json"
+wget "https://the-real-fileio.samitmohnot.repl.co/mcfiles/banned-ips.json"
+wget "https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar"
