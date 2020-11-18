f7b638886310e25e2882ede2b2dd4f0efd034066
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2020-11-18 12:52:35 -0600

    Update windows_installer.py

--- windows/windows_installer.py
+++ windows/windows_installer.py
@@ -20,70 +20,70 @@ print("Done")
 print("Creating EULA(End User[s] Licence Agreement).txt")
 eulac=open("eula.txt", "w+")
 eulae=open("eula.txt", "a+")
-eulae.write("#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).")
-eulae.write("#Fri Sep 04 11:20:58 CDT 2020")
-eulae.write("eula=true")
+eulae.write("""#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Fri Sep 04 11:20:58 CDT 2020
+eula=TRUE""")
 print("Done! (Auto Agreed To The EULA)")
 #Done!
 print("Creating server.properties")
 spc=open("server.properties", "w+")
 spe=open("server.properties", "a+")
-spe.write("#Minecraft server properties")
-spe.write("#Fri Sep 04 11:56:03 CDT 2020")
-spe.write("enable-jmx-monitoring=false")
-spe.write("rcon.port=25575")
-spe.write("level-seed=")
-spe.write("gamemode=survival")
-spe.write("enable-command-block=false")
-spe.write("enable-query=false")
-spe.write("generator-settings=")
-spe.write("level-name=world")
-spe.write("motd=A Minecraft Server")
-spe.write("query.port=25565")
-spe.write("pvp=true")
-spe.write("generate-structures=true")
-spe.write("difficulty=easy")
-spe.write("network-compression-threshold=256")
-spe.write("max-tick-time=60000")
-spe.write("use-native-transport=true")
-spe.write("max-players=20")
-spe.write("online-mode=true")
-spe.write("enable-status=true")
-spe.write("allow-flight=false")
-spe.write("broadcast-rcon-to-ops=true")
-spe.write("view-distance=10")
-spe.write("max-build-height=256")
-spe.write("server-ip=" + ips)
-spe.write("allow-nether=true")
-spe.write("server-port=25565")
-spe.write("enable-rcon=false")
-spe.write("sync-chunk-writes=true")
-spe.write("op-permission-level=4")
-spe.write("prevent-proxy-connections=false")
-spe.write("resource-pack=")
-spe.write("entity-broadcast-range-percentage=100")
-spe.write("rcon.password=")
-spe.write("player-idle-timeout=0")
-spe.write("force-gamemode=false")
-spe.write("rate-limit=0")
-spe.write("hardcore=false")
-spe.write("white-list=false")
-spe.write("broadcast-console-to-ops=true")
-spe.write("spawn-npcs=true")
-spe.write("spawn-animals=true")
-spe.write("snooper-enabled=true")
-spe.write("function-permission-level=2")
-spe.write("level-type=default")
-spe.write("spawn-monsters=true")
-spe.write("enforce-whitelist=false")
-spe.write("resource-pack-sha1=")
-spe.write("spawn-protection=16")
-spe.write("max-world-size=29999984")
+spe.write("""#Minecraft server properties")
+#Fri Sep 04 11:56:03 CDT 2020")
+enable-jmx-monitoring=false
+rcon.port=25575
+level-seed=
+gamemode=survival
+enable-command-block=false
+enable-query=false
+generator-settings=
+level-name=world
+motd=A Minecraft Server
+query.port=25565
+pvp=true
+generate-structures=true
+difficulty=easy
+network-compression-threshold=256
+max-tick-time=60000
+use-native-transport=true
+max-players=20
+online-mode=true
+enable-status=true
+allow-flight=false
+broadcast-rcon-to-ops=true
+view-distance=10
+max-build-height=256
+server-ip=" + ips)
+allow-nether=true
+server-port=25565
+enable-rcon=false
+sync-chunk-writes=true
+op-permission-level=4
+prevent-proxy-connections=false
+resource-pack=
+entity-broadcast-range-percentage=100
+rcon.password=
+player-idle-timeout=0
+force-gamemode=false
+rate-limit=0
+hardcore=false
+white-list=false
+broadcast-console-to-ops=true
+spawn-npcs=true
+spawn-animals=true
+snooper-enabled=true
+function-permission-level=2
+level-type=default
+spawn-monsters=true
+enforce-whitelist=false
+resource-pack-sha1=
+spawn-protection=16
+max-world-size=29999984
 print("Done!!!")
 print("Creating usercache.json")
 ucjc=open("usercache.json", "w+")
 ucje=open("usercache.json", "a+")
-ucje.write("[]")
+ucje.write("[]")""","a+")
 print("Done!")
 print('Creating "Click To Start Minecraft Server"')
 ctsmcsbc=open("Click To Start Minecraft Server.bat", "w+")
