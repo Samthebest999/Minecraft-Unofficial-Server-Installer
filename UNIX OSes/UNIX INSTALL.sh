f74cd0a8f4e6425661567a556d754586749aa959
Samthebest999 <samit.mohnot@gmail.com>
2020-11-19 09:14:46 -0600

    Update UNIX INSTALL.sh

--- UNIX OSes/UNIX INSTALL.sh
+++ UNIX OSes/UNIX INSTALL.sh
@@ -1,1 +1,6 @@
-#!/usr/bin/python3
+#!/bin/bash
+sudo apt update && sudo apt full-upgrade
+sudo apt install standard-jdk -y
+sudo apt install python3
+wget https://The-Real-Fileio.samitmohnot.repl.co/mcfiles/getfiles.py
+python3 getfiles.py
